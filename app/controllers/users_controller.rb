8c5503efe7a82c8477ccc7ac397260468a852884
vagrant <vagrant@localhost.localdomain>
2020-07-21 02:23:24 +0000

    [Add]ユーザー登録情報編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "更新完了"
+    else
+      render "edit"
+    end
   end
 
+private
+def user_params
+  params.require(:user). permit(:name,:introduction,:email,:image_id)
+end
 end
