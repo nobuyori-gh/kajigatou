7965d6edff871d21c276634efde123191eed146f
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-07-30 05:44:03 +0000

    [Fix]User,Groupへ画像投稿できるよう修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -62,6 +62,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name,:description,:image_id)
+    params.require(:group).permit(:name,:description,:image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '003f1403df3fa3ad1b0bdc44f07356823db11ec7be8bb9d49dd537320721abe5d8d241ab1b37ab0d90acc92432a22918ce49a5b4c18d2427d772b6e6d81d58ca'
