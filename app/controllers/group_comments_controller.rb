b93fb83c9838cc2dfe616745a917305b6228c8be
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-08-31 08:57:46 +0000

    [Add]コメント機能　非同期設定

--- app/controllers/group_comments_controller.rb
+++ app/controllers/group_comments_controller.rb
@@ -1,22 +1,24 @@
 class GroupCommentsController < ApplicationController
 
   def create
-    group_task = GroupTask.find(params[:group_task_id])
-    comment = GroupComment.new(group_domment_params)
+    @group_task = GroupTask.find(params[:group_task_id])
+    comment = GroupComment.new(group_comment_params)
     comment.user_id = current_user.id
-    comment.group_task_id = group_task.id
-
+    comment.group_task_id = @group_task.id
     comment.save
-    redirect_to group_task_path(group_task)
+    # redirect_to group_task_path(group_task)
   end
 
   def destroy
-    GroupComment.find_by(id: params[:id], group_task_id: params[:group_task_id]).destroy
-    redirect_to group_task_path(params[:group_task_id])
+    @group_task = GroupTask.find(params[:group_task_id])
+    # GroupComment.find_by(id: params[:id], group_task_id: params[:group_task_id]).destroy
+    # redirect_to group_task_path(params[:group_task_id])
+    comment = GroupComment.find(params[:id])
+    comment.destroy
   end
 
   private
-  def group_domment_params
+  def group_comment_params
     params.require(:group_comment).permit(:comment)
   end
 end
